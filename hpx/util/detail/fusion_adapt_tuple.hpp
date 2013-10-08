8c6e2daf31713b4886a97ff7d745fd42a0fe6af9
K-ballo <k@fusionfenix.com>
2013-10-08 09:59:54 -0500

    tweaks to fusion_adapt_tuple.hpp

--- hpx/util/detail/fusion_adapt_tuple.hpp
+++ hpx/util/detail/fusion_adapt_tuple.hpp
@@ -15,6 +15,7 @@
 ///////////////////////////////////////////////////////////////////////////////
 /// tag_of.hpp
 #include <boost/fusion/support/tag_of_fwd.hpp>
+#include <boost/preprocessor/repetition/enum_params.hpp>
 
 namespace hpx { namespace util { namespace detail
 {
@@ -54,165 +55,45 @@ namespace boost { namespace mpl
 }}
 
 ///////////////////////////////////////////////////////////////////////////////
-/// hpx_tuple_iterator.hpp
-#include <boost/fusion/iterator/iterator_facade.hpp>
-#include <boost/type_traits/is_const.hpp>
-#include <boost/type_traits/remove_const.hpp>
-#include <boost/fusion/support/detail/access.hpp>
-#include <boost/mpl/int.hpp>
-#include <boost/mpl/if.hpp>
-#include <boost/preprocessor/repetition/enum_params.hpp>
-
-namespace boost { namespace fusion
-{
-    struct random_access_traversal_tag;
-
-    template <typename Tuple, int Index>
-    struct hpx_tuple_iterator_identity;
-
-    template <typename Tuple, int Index>
-    struct hpx_tuple_iterator
-        : iterator_facade<
-              hpx_tuple_iterator<Tuple, Index>
-            , random_access_traversal_tag>
-    {
-        typedef Tuple tuple_type;
-        static int const index = Index;
-        typedef hpx_tuple_iterator_identity<
-            typename add_const<Tuple>::type, Index>
-        identity;
-
-        explicit hpx_tuple_iterator(Tuple& tuple)
-          : tuple(tuple) {}
-
-        Tuple& tuple;
-
-        template <typename Iterator>
-        struct value_of
-          : hpx::util::tuple_element<Iterator::index,
-              typename remove_const<typename Iterator::tuple_type>::type> {};
-
-        template <typename Iterator>
-        struct deref
-        {
-            typedef typename value_of<Iterator>::type element;
-            typedef typename
-                mpl::if_<
-                    is_const<typename Iterator::tuple_type>
-                  , typename fusion::detail::cref_result<element>::type
-                  , typename fusion::detail::ref_result<element>::type
-                >::type
-            type;
-
-            static type
-            call(Iterator const& iter)
-            {
-                return hpx::util::get<Index>(iter.tuple);
-            }
-        };
-
-        template <typename Iterator, typename I>
-        struct advance
-        {
-            static int const index = Iterator::index;
-            typedef typename Iterator::tuple_type tuple_type;
-            typedef hpx_tuple_iterator<tuple_type, index+I::value> type;
-
-            static type
-            call(Iterator const& i)
-            {
-                return type(i.tuple);
-            }
-        };
-
-        template <typename Iterator>
-        struct next : advance<Iterator, mpl::int_<1>> {};
-
-        template <typename Iterator>
-        struct prior : advance<Iterator, mpl::int_<-1>> {};
-
-        template <typename I1, typename I2>
-        struct equal_to
-            : is_same<typename I1::identity, typename I2::identity> {};
-
-        template <typename First, typename Last>
-        struct distance
-        {
-            typedef mpl::int_<Last::index-First::index> type;
-
-            static type
-            call(First const&, Last const&)
-            {
-                return type();
-            }
-        };
-    };
-}}
-
-///////////////////////////////////////////////////////////////////////////////
-/// at_impl.hpp
-#include <boost/mpl/if.hpp>
-#include <boost/fusion/support/detail/access.hpp>
-#include <boost/type_traits/remove_const.hpp>
+/// is_sequence_impl.hpp
+#include <boost/mpl/bool.hpp>
 
 namespace boost { namespace fusion
 {
     namespace extension
     {
-        template<typename T>
-        struct at_impl;
+        template<typename Tag>
+        struct is_sequence_impl;
 
-        template <>
-        struct at_impl<hpx::util::detail::tuple_tag>
+        template<>
+        struct is_sequence_impl<hpx::util::detail::tuple_tag>
         {
-            template <typename Sequence, typename I>
+            template<typename Sequence>
             struct apply
-            {
-                typedef typename remove_const<Sequence>::type seq_type;
-                typedef typename hpx::util::tuple_element<I::value, seq_type>::type element;
-
-                typedef typename
-                    mpl::if_<
-                        is_const<Sequence>
-                      , typename fusion::detail::cref_result<element>::type
-                      , typename fusion::detail::ref_result<element>::type
-                    >::type
-                type;
-
-                static type
-                call(Sequence& seq)
-                {
-                    return hpx::util::get<I::value>(seq);
-                }
-            };
+              : mpl::true_
+            {};
         };
     }
 }}
 
 ///////////////////////////////////////////////////////////////////////////////
-/// begin_impl.hpp
+/// is_view_impl.hpp
+#include <boost/mpl/bool.hpp>
 
 namespace boost { namespace fusion
 {
     namespace extension
     {
-        template<typename T>
-        struct begin_impl;
+        template<typename Tag>
+        struct is_view_impl;
 
-        template <>
-        struct begin_impl<hpx::util::detail::tuple_tag>
+        template<>
+        struct is_view_impl<hpx::util::detail::tuple_tag>
         {
-            template <typename Sequence>
+            template<typename T>
             struct apply
-            {
-                typedef hpx_tuple_iterator<Sequence, 0> type;
-
-                static type
-                call(Sequence& v)
-                {
-                    return type(v);
-                }
-            };
+              : mpl::false_
+            {};
         };
     }
 }}
@@ -242,8 +123,166 @@ namespace boost { namespace fusion
 }}
 
 ///////////////////////////////////////////////////////////////////////////////
+/// hpx_tuple_iterator.hpp
+#include <hpx/util/add_lvalue_reference.hpp>
+#include <boost/fusion/iterator/iterator_facade.hpp>
+#include <boost/mpl/int.hpp>
+
+namespace boost { namespace fusion
+{
+    struct random_access_traversal_tag;
+
+    template <typename Tuple, int Index>
+    struct hpx_tuple_iterator_identity;
+
+    template <typename Tuple, int Index>
+    struct hpx_tuple_iterator
+      : iterator_facade<
+            hpx_tuple_iterator<Tuple, Index>
+          , random_access_traversal_tag
+        >
+    {
+        typedef Tuple tuple_type;
+        static int const index = Index;
+        typedef hpx_tuple_iterator_identity<
+            typename add_const<Tuple>::type, Index>
+            identity;
+
+        explicit hpx_tuple_iterator(Tuple& tuple)
+          : tuple(tuple) {}
+
+        Tuple& tuple;
+
+        template <typename Iterator>
+        struct value_of
+          : hpx::util::tuple_element<
+                Iterator::index
+              , typename Iterator::tuple_type
+            >
+        {};
+
+        template <typename Iterator>
+        struct deref
+        {
+            typedef typename 
+                hpx::util::tuple_element<
+                    Iterator::index
+                  , typename Iterator::tuple_type
+                >::type
+                element;
+            typedef typename
+                hpx::util::add_lvalue_reference<element>::type
+                type;
+
+            static type call(Iterator const& iter)
+            {
+                return hpx::util::get<Index>(iter.tuple);
+            }
+        };
+
+        template <typename Iterator, typename I>
+        struct advance
+        {
+            static int const index = Iterator::index;
+            typedef typename Iterator::tuple_type tuple_type;
+            typedef hpx_tuple_iterator<tuple_type, index+I::value> type;
+
+            static type call(Iterator const& i)
+            {
+                return type(i.tuple);
+            }
+        };
+
+        template <typename Iterator>
+        struct next
+          : advance<Iterator, mpl::int_<1>>
+        {};
+
+        template <typename Iterator>
+        struct prior
+          : advance<Iterator, mpl::int_<-1>>
+        {};
+
+        template <typename I1, typename I2>
+        struct equal_to
+          : is_same<typename I1::identity, typename I2::identity>
+        {};
+
+        template <typename First, typename Last>
+        struct distance
+        {
+            typedef mpl::int_<Last::index-First::index> type;
+
+            static type call(First const&, Last const&)
+            {
+                return type();
+            }
+        };
+    };
+}}
+
+///////////////////////////////////////////////////////////////////////////////
+/// at_impl.hpp
+#include <hpx/util/add_lvalue_reference.hpp>
+
+namespace boost { namespace fusion
+{
+    namespace extension
+    {
+        template<typename T>
+        struct at_impl;
+
+        template <>
+        struct at_impl<hpx::util::detail::tuple_tag>
+        {
+            template <typename Sequence, typename I>
+            struct apply
+            {
+                typedef typename 
+                    hpx::util::tuple_element<I::value, Sequence>::type
+                    element;
+                typedef typename
+                    hpx::util::add_lvalue_reference<element>::type
+                    type;
+
+                static type call(Sequence& seq)
+                {
+                    return hpx::util::get<I::value>(seq);
+                }
+            };
+        };
+    }
+}}
+
+///////////////////////////////////////////////////////////////////////////////
+/// begin_impl.hpp
+
+namespace boost { namespace fusion
+{
+    namespace extension
+    {
+        template<typename T>
+        struct begin_impl;
+
+        template <>
+        struct begin_impl<hpx::util::detail::tuple_tag>
+        {
+            template <typename Sequence>
+            struct apply
+            {
+                typedef hpx_tuple_iterator<Sequence, 0> type;
+
+                static type call(Sequence& v)
+                {
+                    return type(v);
+                }
+            };
+        };
+    }
+}}
+
+///////////////////////////////////////////////////////////////////////////////
 /// end_impl.hpp
-#include <boost/type_traits/remove_const.hpp>
 
 namespace boost { namespace fusion
 {
@@ -258,12 +297,10 @@ namespace boost { namespace fusion
             template <typename Sequence>
             struct apply
             {
-                typedef typename remove_const<Sequence>::type seq_type;
-                static int const size = hpx::util::tuple_size<seq_type>::value;
+                static int const size = hpx::util::tuple_size<Sequence>::value;
                 typedef hpx_tuple_iterator<Sequence, size> type;
 
-                static type
-                call(Sequence& v)
+                static type call(Sequence& v)
                 {
                     return type(v);
                 }
@@ -273,49 +310,8 @@ namespace boost { namespace fusion
 }}
 
 ///////////////////////////////////////////////////////////////////////////////
-/// is_sequence_impl.hpp
-#include <boost/mpl/bool.hpp>
-
-namespace boost { namespace fusion
-{
-    namespace extension
-    {
-        template<typename Tag>
-        struct is_sequence_impl;
-
-        template<>
-        struct is_sequence_impl<hpx::util::detail::tuple_tag>
-        {
-            template<typename Sequence>
-            struct apply : mpl::true_ {};
-        };
-    }
-}}
-
-///////////////////////////////////////////////////////////////////////////////
-/// is_view_impl.hpp
-#include <boost/mpl/bool.hpp>
-
-namespace boost { namespace fusion
-{
-    namespace extension
-    {
-        template<typename Tag>
-        struct is_view_impl;
-
-        template<>
-        struct is_view_impl<hpx::util::detail::tuple_tag>
-        {
-            template<typename T>
-            struct apply : mpl::false_ {};
-        };
-    }
-}}
-
-///////////////////////////////////////////////////////////////////////////////
 /// size_impl.hpp
 #include <boost/mpl/int.hpp>
-#include <boost/type_traits/remove_const.hpp>
 
 namespace boost { namespace fusion
 {
@@ -328,10 +324,8 @@ namespace boost { namespace fusion
         struct size_impl<hpx::util::detail::tuple_tag>
         {
             template <typename Sequence>
-            struct apply :
-                mpl::int_<hpx::util::tuple_size<
-                    typename remove_const<Sequence>::type>::value
-                >
+            struct apply
+              : mpl::int_<hpx::util::tuple_size<Sequence>::value>
             {};
         };
     }
@@ -350,7 +344,9 @@ namespace boost { namespace fusion
         struct value_at_impl<hpx::util::detail::tuple_tag>
         {
             template <typename Sequence, typename I>
-            struct apply : hpx::util::tuple_element<I::value, Sequence> {};
+            struct apply
+              : hpx::util::tuple_element<I::value, Sequence>
+            {};
         };
     }
 }}
